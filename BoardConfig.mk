7043ad144865e128b53f2891368f2bde5838db99
Ririshi <andele.rien@gmail.com>
2014-08-21 20:31:40 +0200

    Commented out the Wi-Fi lines as a dirty compiler workaround

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,14 +107,14 @@ TARGET_SPECIFIC_HEADER_PATH := device/htc/golfu/include
 BOARD_EGL_CFG := device/htc/golfu/proprietary/lib/egl/egl.cfg
 
 # Wi-Fi
-BOARD_WLAN_DEVICE := ath6kl
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
-BOARD_HOSTAPD_DRIVER := NL80211
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl_sdio.ko"
-WIFI_DRIVER_MODULE_NAME := "ath6kl_sdio"
+#BOARD_WLAN_DEVICE := ath6kl
+#BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_ath6kl
+#BOARD_HOSTAPD_DRIVER := NL80211
+#WPA_SUPPLICANT_VERSION := VER_0_8_X
+#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_ath6kl
+#WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/ath6kl_sdio.ko"
+#WIFI_DRIVER_MODULE_NAME := "ath6kl_sdio"
 
 # Misc
 TARGET_BOOTANIMATION_PRELOAD := true
